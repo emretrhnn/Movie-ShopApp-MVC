cd8a366a14af11f9f57c6b9f17ae871f82854a94
emretrhnn <emretrhn3@gmail.com>
2023-10-30 23:19:11 +0300

    v5-login

--- ShopApp/MovieShopApp.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MovieShopApp.WebUI.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
