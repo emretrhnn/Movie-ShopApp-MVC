08fa54edca8e8cdc87849af4337ff74d801e0047
emretrhnn <emretrhn3@gmail.com>
2023-10-30 21:50:03 +0300

    v3

---
+++ ShopApp/Movie.Business/Models/MovieListModel 2.cs
@@ -0,0 +1,13 @@
+﻿#nullable disable
+
+using Movie.DataAccess.Entities;
+
+namespace Movie.Business.Models
+{
+    //the model I will use for listing
+    public class MovieListModel
+	{
+		public List<SilverScreen> silverScreens { get; set; }
+	}
+}
+
---
+++ ShopApp/Movie.DataAccess/Entities/Role 2.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+
+
+using System.ComponentModel.DataAnnotations;
+using Core.Records.Bases;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Movie.DataAccess.Entities
+{
+	public class Role : Record
+	{
+		[Required]
+        [StringLength(10)]
+        public string Name { get; set; }
+
+		public List<User> Users { get; set; } //for 1 to many relationships
+
+    }
+}
+
